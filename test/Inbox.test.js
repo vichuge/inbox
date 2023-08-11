466b4a0f8e183efaf3528bc344e75a000fc82d5a
vichuge <victor.hugo.pacheco.flores@gmail.com>
2023-08-10 18:56:59 -0600

    44 add a web3 variable

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,8 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 
+const assert = require('assert');
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
+const web3 = new Web3(ganache.provider());
