96d67ed9615ab7b6ffaad022f5a2672f60bb6527
vichuge <victor.hugo.pacheco.flores@gmail.com>
2023-08-21 14:20:29 -0600

    Test mocha car example

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,3 +6,29 @@ const assert = require('assert');
 
 // contract test code will go here
 const web3 = new Web3(ganache.provider());
+
+class Car {
+  park() {
+    return 'stopped';
+  }
+
+  drive() {
+    return 'vroom';
+  }
+}
+
+let car;
+
+beforeEach(() => {
+  car = new Car();
+});
+
+describe('Car', () => {
+  it('can park', () =>{
+    assert.equal(car.park(), 'stopped');
+  });
+
+  it('can drive', () =>{
+    assert.equal(car.drive(), 'vroom');
+  });
+});
